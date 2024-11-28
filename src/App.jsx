b66466afaacd2426bb769aa370cc66046fb1248b
Preet01001 <sharmapunit145@gmail.com>
2024-11-28 21:21:39 +0530

    yutu

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import React, { useEffect, useState,useRef } from 'react';
 import { io } from 'socket.io-client';
 import {Peer} from "peerjs"
+import Splash from './Splash';
+import "./App.css"
 
 
-      let socket = io('https://video-node-4zbi.onrender.com/');
+      let socket = io('http://localhost:1010');
       let peer = new Peer();
   function App() {
     const [partnerId, setPartnerId] = useState(null);
     let [peerId,setPeerId] = useState();
     let[userPeer,setUserPeer] = useState();
+    let[loading,setLoading] = useState(true);
     
     let localVideoRef = useRef();
     let remoteVideoRef = useRef();
+
+    useEffect(()=>{
+      let timer = setTimeout(()=>{
+        setLoading(false);
+      },1000);
+      return ()=>{
+        clearTimeout(timer);
+      }
+    })
    
     useEffect(() => {
       if(socket && peer){
@@ -38,8 +50,9 @@ import {Peer} from "peerjs"
                 call.on("stream",(remoteStream)=>{
                   remoteVideoRef.current.srcObject = remoteStream;
                 })
+              }).catch((er)=>{
+                 console.log(er)
               })
-              
              })
 
         return () => {
@@ -79,49 +92,70 @@ import {Peer} from "peerjs"
   navigator.mediaDevices.getUserMedia({video:true,audio:true}).then((stream)=>{
     localVideoRef.current.srcObject = stream;
     
+    
     let call = peer.call(userPeer,stream);
      call.on("stream",(remoteStream)=>{
       remoteVideoRef.current.srcObject = remoteStream;
      })
+  }).catch((er)=>{
+    console.log(er)
   })
  }
 
-
- const videoStyle = {
-  width: '48%',
-  
-  borderRadius: '10px',
-  backgroundColor: 'black',
-};
-
-const buttonStyle = {
-  padding: '10px 20px',
-  backgroundColor: '#007BFF',
-  color: 'white',
-  border: 'none',
-  borderRadius: '5px',
-};
-
+     if(loading){
+    return <Splash/>
+        }
 
     return (
-        <div>
-            <h1>Socket.io Pairing</h1>
-            {partnerId ? (<>
-                <p>Paired with user: {partnerId}</p>
-                   <video ref={localVideoRef} style={videoStyle} autoPlay></video>
-                   <br/>
-                   <video ref={remoteVideoRef} autoPlay style={videoStyle}></video>
-                   <br/>
-                   <button onClick={call} style={buttonStyle}>call</button>
-                   <br/>
-                   <button onClick={re}>next</button>
-                   </>
-            ) : (
-                <p>Waiting for a partner...</p>
-            )}
-
-           
+      <div>
+            <nav className="navbar">
+        <div className="navbar-logo">
+          <img src="image.png" alt="RandomMeet Logo" />
         </div>
+      </nav>
+
+      {partnerId ? (
+        
+          <div className="container">
+          
+              <div className="video-container">
+                  <video ref={localVideoRef} muted className="video" autoPlay></video>
+                  <br />
+                  <video ref={remoteVideoRef} autoPlay className="video"></video>
+              </div>
+             
+              <div className="button-container">
+                  <button onClick={call} className="button">Call</button>
+                  <button onClick={re} className="button">Next</button>
+              </div>
+              <div className='text-container'>
+              <p><img src='image.png'/></p>
+              <p >Welcome to RandomMeet, a random webcam chat app made to anonymously talk to strangers online. Choose between our 1-on-1 video chat roulette to instantly meet new people.</p>
+              <img src='th (19).jpg'/>
+              <p>
+<br/><b>Connect and Chat:</b> After being connected, you can start a video chat with your match instantly (hitting the call button). You’ll have the option to continue chatting or find a new match (next button).
+
+<br/><b>Stay Safe:</b> We prioritize your privacy and safety. You can choose to disconnect from a chat at any time and report any inappropriate behavior.
+
+<br/><b>Enjoy Your Conversations:</b> RandomMeet is designed to help you meet new people and have interesting conversations. Have fun and make new connections!</p>
+              </div>
+              <div className="contact-container">
+  <p>Contact and Follow us on:</p>
+  <div className="icon-container">
+   <a><img src="instagram-1.png" alt="Instagram" /></a> 
+   <a 
+        href="https://wa.me/918439497883" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >  <img src="Whatsapp-Logo-Png-Transparent-Background.png" alt="WhatsApp" /></a>
+  </div>
+</div>
+
+          </div>
+      ) : (
+          <p className="waiting">Waiting for a partner...</p>
+      )}
+  </div>
     );
 }
 
---
+++ src/Splash.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Splash.css"
+
+function Splash() {
+  return (
+    
+    <div class="logo-container">
+    <div class="camera-icon">
+      <div class="camera-lens"></div>
+      <div class="camera-light"></div>
+    </div>
+    <div class="logo-text">RandomMeet</div>
+  </div>
+  )
+}
+
+export default Splash
